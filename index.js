ca4d4d2ea51cdeefa416d6705272f402495005f7
mitishaaaa <119412188+mitishaaaa@users.noreply.github.com>
2023-04-17 00:11:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImageSource = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImageSource)
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImageSource = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImageSource)
+
+if (randomNumber1 > randomNumber2 ) {
+    document.querySelector("h1").innerHTML = "🎉 Player 1 Wins!";
+}
+
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "🎉 Player 2 Wins!";
+}
+
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
